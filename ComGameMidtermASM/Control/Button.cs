262c078147ad40e8944ba0dbb1aecae1ee60355c
21 September <Thammanit.ch@gmail.com>
2022-03-10 03:51:46 +0700

    Do button not bg yet

---
+++ ComGameMidtermASM/Control/Button.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ComGameMidtermASM.Control
+{
+    public class Button : Component
+    {
+        #region Fields
+
+        private MouseState _currentMouseState;
+
+        //private SpriteFont _font;
+
+        private bool _isHovering;
+
+        private MouseState _previousMouseState;
+
+        private Texture2D _texture;
+
+
+        #endregion
+
+        #region Properties
+
+        public event EventHandler Click;
+
+        public string Text { get; set; }
+        public bool Clicked{ get; private set; }
+
+        public Color PenColor { get; set; }
+
+        public Vector2 Position { get; set; }
+
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height);
+            }
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        public Button(Texture2D texture)
+        {
+            _texture = texture;
+            //_font = font;
+        }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            //declare idle button state
+            var colour = Color.White;
+            if (_isHovering)
+            {
+                _texture = start_hover_ori;
+            }
+
+            spriteBatch.Draw(_texture, Rectangle, colour);
+
+            if (!string.IsNullOrEmpty(Text))
+            {
+                var x = (Rectangle.X + (Rectangle.Width / 2));
+                var y = (Rectangle.X + (Rectangle.Height / 2));
+
+                //spriteBatch.DrawString(_font, Text, new Vector2(x, y), PenColor);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _previousMouseState = _currentMouseState;
+            _currentMouseState = Mouse.GetState();
+
+            var mouseRectangle = new Rectangle(_currentMouseState.X, _currentMouseState.Y, 1, 1);
+
+            _isHovering = false;
+
+            if (mouseRectangle.Intersects(Rectangle))
+            {
+                _isHovering = true;
+
+                if (_currentMouseState.LeftButton == ButtonState.Released && _previousMouseState.LeftButton == ButtonState.Pressed)
+                {
+                    Click?.Invoke(this, new EventArgs());
+
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- ComGameMidtermASM/Game.cs
+++ ComGameMidtermASM/Game.cs
@@ -1,55 +1,120 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using ComGameMidtermASM.Control;
 
 namespace ComGameMidtermASM
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Color _backgroundColour = Color.CornflowerBlue;
+
+        private List<Component> _gameComponents;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
         }
 
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            IsMouseVisible = true;
 
-            // determine window size. from Singleton class
-            _graphics.PreferredBackBufferHeight = Singleton.SCREENHEIGHT;
-            _graphics.PreferredBackBufferWidth = Singleton.SCREENWIDTH;
-            _graphics.ApplyChanges();
-            //
             base.Initialize();
         }
 
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            var randomButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(350, 200),
+                Text = "Random",
+            };
+
+            randomButton.Click += RandomButton_Click;
+
+            var quitButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(350, 250),
+                Text = "Quit",
+            };
+
+            quitButton.Click += QuitButton_Click;
+
+            _gameComponents = new List<Component>()
+      {
+        randomButton,
+        quitButton,
+      };
         }
 
+        private void QuitButton_Click(object sender, System.EventArgs e)
+        {
+            Exit();
+        }
+
+        private void RandomButton_Click(object sender, System.EventArgs e)
+        {
+            var random = new Random();
+
+            _backgroundColour = new Color(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
+            foreach (var component in _gameComponents)
+                component.Update(gameTime);
 
             base.Update(gameTime);
         }
 
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(_backgroundColour);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            foreach (var component in _gameComponents)
+                component.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
