b0b625a5aed7c9c4277fa39e9eb85113234b4c9b
21 September <Thammanit.ch@gmail.com>
2022-03-10 15:54:30 +0700

    Update button and making menu

--- ComGameMidtermASM/Control/Button.cs
+++ ComGameMidtermASM/Control/Button.cs
@@ -11,16 +11,15 @@ namespace ComGameMidtermASM.Control
     {
         #region Fields
 
-        private MouseState _currentMouseState;
+        private MouseState _currentMouse;
 
-        //private SpriteFont _font;
+        private SpriteFont _font;
 
         private bool _isHovering;
 
-        private MouseState _previousMouseState;
+        private MouseState _previousMouse;
 
         private Texture2D _texture;
-
 
         #endregion
 
@@ -28,10 +27,9 @@ namespace ComGameMidtermASM.Control
 
         public event EventHandler Click;
 
-        public string Text { get; set; }
-        public bool Clicked{ get; private set; }
+        public bool Clicked { get; private set; }
 
-        public Color PenColor { get; set; }
+        public Color PenColour { get; set; }
 
         public Vector2 Position { get; set; }
 
@@ -43,42 +41,45 @@ namespace ComGameMidtermASM.Control
             }
         }
 
+        public string Text { get; set; }
 
         #endregion
 
         #region Methods
 
-        public Button(Texture2D texture)
+        public Button(Texture2D texture, SpriteFont font)
         {
             _texture = texture;
-            //_font = font;
+
+            _font = font;
+
+            PenColour = Color.Black;
         }
+
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            //declare idle button state
             var colour = Color.White;
+
             if (_isHovering)
-            {
-                _texture = start_hover_ori;
-            }
+                colour = Color.Gray;
 
             spriteBatch.Draw(_texture, Rectangle, colour);
 
             if (!string.IsNullOrEmpty(Text))
             {
-                var x = (Rectangle.X + (Rectangle.Width / 2));
-                var y = (Rectangle.X + (Rectangle.Height / 2));
+                var x = (Rectangle.X + (Rectangle.Width / 2)) - (_font.MeasureString(Text).X / 2);
+                var y = (Rectangle.Y + (Rectangle.Height / 2)) - (_font.MeasureString(Text).Y / 2);
 
-                //spriteBatch.DrawString(_font, Text, new Vector2(x, y), PenColor);
+                spriteBatch.DrawString(_font, Text, new Vector2(x, y), PenColour);
             }
         }
 
         public override void Update(GameTime gameTime)
         {
-            _previousMouseState = _currentMouseState;
-            _currentMouseState = Mouse.GetState();
+            _previousMouse = _currentMouse;
+            _currentMouse = Mouse.GetState();
 
-            var mouseRectangle = new Rectangle(_currentMouseState.X, _currentMouseState.Y, 1, 1);
+            var mouseRectangle = new Rectangle(_currentMouse.X, _currentMouse.Y, 1, 1);
 
             _isHovering = false;
 
@@ -86,10 +87,9 @@ namespace ComGameMidtermASM.Control
             {
                 _isHovering = true;
 
-                if (_currentMouseState.LeftButton == ButtonState.Released && _previousMouseState.LeftButton == ButtonState.Pressed)
+                if (_currentMouse.LeftButton == ButtonState.Released && _previousMouse.LeftButton == ButtonState.Pressed)
                 {
                     Click?.Invoke(this, new EventArgs());
-
                 }
             }
         }
---
+++ ComGameMidtermASM/State/MenuState.cs
@@ -0,0 +1,82 @@
+﻿using ComGameMidtermASM.Control;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComGameMidtermASM.State
+{
+    public class MenuState : State
+    {
+        private List<Component> _components;
+
+        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            var buttonTextuere = _content.Load<Texture2D>("Control/start_idle");
+            var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
+            var newGameButton = new Button(buttonTextuere, buttonFont)
+            {
+                Position = new Vector2(300, 200),
+                Text = "New Game",
+            };
+
+            newGameButton.Click += newGameButton_Click;
+
+            var loadGameButton = new Button(buttonTextuere, buttonFont)
+            {
+                Position = new Vector2(300, 250),
+                Text = "Load Game",
+            };
+
+            loadGameButton.Click += loadGameButton_Click;
+
+            var quitGameButton = new Button(buttonTextuere, buttonFont)
+            {
+                Position = new Vector2(300, 300),
+                Text = "Quit Game",
+            };
+
+            quitGameButton.Click += quitGameButton_Click;
+
+            _components = new List<Component>
+            {
+
+            };
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+        private void loadGameButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void quitGameButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
